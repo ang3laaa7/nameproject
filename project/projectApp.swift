436efc0fa0028ff58a95e91242bb8520bad86f10
ang3laaa7 <angelachanc321@gmail.com>
2024-06-21 13:24:00 -0400

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Angela on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
